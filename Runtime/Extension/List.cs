9e55b7404c23786ef0297b9f46adfdd1bb6dea75
LeKAKiD <lekakid@kakao.com>
2023-12-11 01:48:17 +0900

    add List.Shuffle(int seed)

--- Runtime/Extension/List.cs
+++ Runtime/Extension/List.cs
@@ -1,16 +1,20 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using Random = UnityEngine.Random;
 
 namespace LeKAKiD.List {
     public static class ListExtension {
-        private static readonly Random rand = new Random();
-
         public static void Shuffle<T>(this IList<T> list) {
+            Shuffle(list, (int)DateTime.Now.Ticks);
+        }
+
+        public static void Shuffle<T>(this IList<T> list, int seed) {
+            Random.InitState(seed);
+
             int n = list.Count;
             while (n > 1) {
                 n -= 1;
-                int k = rand.Next(n + 1);
+                int k = Random.Range(0, n + 1);
                 T value = list[k];
                 list[k] = list[n];
                 list[n] = value;
