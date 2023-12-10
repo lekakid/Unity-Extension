ccdf28da3cabf6128d7d7069da5065cfccd3b4fc
LeKAKiD <lekakid@kakao.com>
2023-12-10 23:30:03 +0900

    Add Dictionary Extension

---
+++ Runtime/Extension/Dictionary.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LeKAKiD.Dictionary {
+    public static class DictionaryExtension {
+        public static string GetEntriesString<K, V>(this IDictionary<K, V> dictionary) {
+            List<string> entryStringList = new List<string>();
+
+            var enumerator = dictionary.GetEnumerator();
+            while (enumerator.MoveNext()) {
+                entryStringList.Add(enumerator.Current.ToString());
+            }
+
+            return string.Join(", ", entryStringList);
+        }
+    }
+}
