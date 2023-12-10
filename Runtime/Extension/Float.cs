577383bc50aa17cfeb888ea350757a509127ddc9
LeKAKiD <lekakid@kakao.com>
2023-12-11 00:07:15 +0900

    Add Float Extension

---
+++ Runtime/Extension/Float.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace LeKAKiD.Float {
+    public static class FloatExtension {
+        public static float Remap(this float value, float prevA, float prevB, float nextA = 0, float nextB = 1) {
+            float normalizedValue = Mathf.InverseLerp(prevA, prevB, value);
+            return Mathf.Lerp(nextA, nextB, normalizedValue);
+        }
+    }
+}
